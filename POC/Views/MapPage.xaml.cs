cb8c280ae8dcc4035816d51f1726c8144e7ec91b
Andrew <ahr.andrew@gmail.com>
2020-04-18 23:07:40 -0400

    added platform specific permissions for location and map geolocation functionality

--- POC/Views/MapPage.xaml.cs
+++ POC/Views/MapPage.xaml.cs
@@ -2,6 +2,7 @@
 using Xamarin.Forms;
 using Xamarin.Forms.Maps;
 using Xamarin.Forms;
+using Xamarin.Essentials;
 
 namespace POC.Views
 {
@@ -12,31 +13,48 @@ namespace POC.Views
             InitializeComponent();
         }
 
+        protected override async void OnAppearing()
+        {
+            map.IsShowingUser = true;
+            var request = new GeolocationRequest(GeolocationAccuracy.Medium);
+            var location = await Geolocation.GetLocationAsync(request);
+
+            if (location == null)
+            {
+                location = await Geolocation.GetLastKnownLocationAsync(); 
+            }
+
+            map.MoveToRegion(MapSpan.FromCenterAndRadius(new Position(location.Latitude, location.Longitude), Distance.FromMiles(1)));
+
+        }
+
+
         void OnSliderValueChanged(object sender, ValueChangedEventArgs e)
         {
             double zoomLevel = e.NewValue;
             double latlongDegrees = 360 / (Math.Pow(2, zoomLevel));
-            if(map.VisibleRegion != null)
+            if (map.VisibleRegion != null)
             {
                 map.MoveToRegion(new MapSpan(map.VisibleRegion.Center, latlongDegrees, latlongDegrees));
             }
         }
 
+
         void OnButtonClicked(object sender, EventArgs e)
         {
             Button button = sender as Button;
-            //switch (button.Text)
-      //      {
-       //         case "Street":
-     //               map.MapType = map.Street;
-       //             break;
-    //            case "Satellite":
-     //               map.MapType = map.Satellite;
-         //           break;
-       //         case "Hybrid":
-         //           map.MapType = map.Hybrid;
-          //          break;
-         //   }
+            switch (button.Text)
+              {
+                  case "Street":
+                    map.MapType = MapType.Street;
+                    break;
+                 case "Satellite":
+                     map.MapType = MapType.Satellite;
+                     break;
+                 case "Hybrid":
+                     map.MapType = MapType.Hybrid;
+                    break;
+            }
         }
 
 
